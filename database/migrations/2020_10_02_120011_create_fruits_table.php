f4155f679d5519e6055defb91436e33c16697039
Kevin Maikath <kevinrx25@gmail.com>
2020-10-02 13:27:07 +0100

    Fruit model migration

---
+++ database/migrations/2020_10_02_120011_create_fruits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFruitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fruits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->enum('size', ['small', 'medium', 'large']);
+            $table->string('colour');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fruits');
+    }
+}
